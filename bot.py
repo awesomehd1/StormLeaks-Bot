1a0658afa0b6720c6e416f9fc674d1cdd1da0a8d
awesomehd1 <earthgood83@gmail.com>
2018-09-08 20:25:25 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,4 @@ bot = commands.Bot(command_prefix='!')
 async def tomorrow(ctx):
     await ctx.send("tomorrow's shop https://stormleaks.tk/shopimage/screenshot-fnbr.co-2018-09-08-20-05-59-330.png")
 
-@bot.command()
-async def today(ctx)
-    await ctx.send("today's shop https://cdn.discordapp.com/attachments/440259640652202004/488136797579378688/shop-9-9-2018.png")
-
 bot.run('NDg4MTg0NzMwNjQ3Mzk2Mzcz.DnYhCA.fxGnlBgyLbsrn90mDwUB2wGPkdo')
