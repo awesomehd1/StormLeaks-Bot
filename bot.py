411283ad7259efd096b304229a152982d0639588
awesomehd1 <earthgood83@gmail.com>
2018-09-08 20:12:25 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,4 @@ bot = commands.Bot(command_prefix='!')
 async def tomorrow(ctx):
     await ctx.send("tomorrow's shop https://stormleaks.tk/shopimage/screenshot-fnbr.co-2018-09-08-20-05-59-330.png")
 
-bot.run('<YOUR_TOKEN_HERE>')
+bot.run('NDg4MTg0NzMwNjQ3Mzk2Mzcz.DnYhCA.fxGnlBgyLbsrn90mDwUB2wGPkdo')
